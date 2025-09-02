a1866ab5986ea692ac897650f00127696563dd7b
discountchubbs <alexgrant990@gmail.com>
2025-09-02 14:59:10 -0700

    modeld_v2: desire rename and add many parts from thneed modeld #1197:
    commit c80af972c3bd78cec6f7851d54fe20a162696a97
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Mon Sep 1 10:16:04 2025 -0700
    
        Refactor buffer handling for temporal inputs and streamline desire updates
    
    commit 813fc1807bd3cbf0aab3fd39f0ba6bbd31561f71
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Sun Aug 31 16:06:05 2025 -0700
    
        loop more models
    
    commit a5aafb3411d2d18336eb69a974962167ffe3d499
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Sun Aug 31 16:02:15 2025 -0700
    
        Refactor shape mode parameters for desire handling in test buffer logic
    
    commit 663d14c170a8c4d960a4b556e0e40a74dbe76f8c
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Sun Aug 31 13:27:43 2025 -0700
    
        meh
    
    commit 50c8aa92cf7319ad4efbd2bf03b6ef96efa9a259
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Sun Aug 31 13:24:48 2025 -0700
    
        Bump selector version to 10
    
    commit ae5f4fcc647baddddfe8efdf5f002fc096f21b30
    Author: discountchubbs <alexgrant990@gmail.com>
    Date:   Thu Aug 28 17:20:01 2025 -0500
    
        Add model metadata lookup and update desire handling

--- selfdrive/ui/sunnypilot/qt/offroad/settings/visuals_panel.cc
+++ selfdrive/ui/sunnypilot/qt/offroad/settings/visuals_panel.cc
@@ -38,14 +38,7 @@ VisualsPanel::VisualsPanel(QWidget *parent) : QWidget(parent) {
       tr("Enable the rainbow mode, which displays a rainbow effect on the screen."),
       "../assets/offroad/icon_monitoring.png",
       false,
-    },
-    {
-      "RainbowMode",
-      tr("Enable Tesla Rainbow Mode"),
-      RainbowizeWords(tr("A beautiful rainbow effect on the path the model wants to take.")) + "<br/><i>" + tr("It")+ " <b>" + tr("does not") + "</b> " + tr("affect driving in any way.") + "</i>",
-      "../assets/offroad/icon_monitoring.png",
-      false,
-    },
+    }
   };
 
   for (auto &[param, title, desc, icon, needs_restart] : toggle_defs) {
